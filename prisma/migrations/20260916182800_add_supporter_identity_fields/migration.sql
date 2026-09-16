0880001b34f3addff6d325322ec7e526e06aa29e
Richi Beja <richibeja@gmail.com>
2026-09-16 18:30:05 -0500

    Feat: Add HTTP Basic Auth middleware and prepare identity schema

---
+++ prisma/migrations/20260916182800_add_supporter_identity_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Support" ADD COLUMN     "avatarUrl" TEXT,
+ADD COLUMN     "channelName" TEXT,
+ADD COLUMN     "channelUrl" TEXT,
+ADD COLUMN     "displayName" TEXT,
+ADD COLUMN     "handle" TEXT;
